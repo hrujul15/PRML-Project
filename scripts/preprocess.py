a14487f336601b62b208b0a20469afdf3688145b
Primz007 <patelpriyam2006@gmail.com>
2026-04-08 22:11:38 +0530

    model

---
+++ scripts/preprocess.py
@@ -0,0 +1,29 @@
+import os
+from spectrogram import generate_spectrogram
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+
+def preprocess():
+    data = "data/genres_original"
+    x = []
+    y = []
+
+    # loop over folder and sub-folder
+    for genre_folder in os.scandir(data):
+        for files in os.scandir(genre_folder):
+            # get sample
+            sample = files.name
+            spectogram = generate_spectrogram(sample)
+            # if corrupt then dont add
+            if spectogram is not None:
+                x.append(spectogram)
+                y.append(sample[:sample.find(".")])
+    
+    # get string to num maping
+    mapping = LabelEncoder()
+    y = mapping.fit_transform(y)
+    mapping = dict(zip(range(len(mapping.classes_)), mapping.classes_))
+    x = np.array(x)
+    
+    
+    return x,y,mapping
--- scripts/spectrogram.py
+++ scripts/spectrogram.py
@@ -5,17 +5,27 @@ from moviepy import VideoFileClip
 import librosa
 
 def generate_spectrogram(file_path):
-    if(".wav" not in file_path):
-        # Step 1: convert to wav if not in the format
-        clip = VideoFileClip(file_path)
-        clip.audio.write_audiofile("out_audio.wav")
-        file_path="out_audio.wav"
-    y, sr = librosa.load(file_path) # sr = sample rate
-    y = y[:int(sr*30)]
-    # Step 2: do fft over the audio file and convert output to mel scale
-    mel_spect = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=1024)
-    # # Convert amplitude to decibels so that difference in loudness is distinguishable
-    mel_spect = librosa.power_to_db(mel_spect, ref=np.max)
-    # getting all values between 0 and 1
-    mel_spect = (mel_spect - mel_spect.min()) / (mel_spect.max() - mel_spect.min())
-    return mel_spect
+    file_path =  "data/genres_original/" + file_path[:file_path.find(".")] + "/" + file_path
+    # some file in data set are corrupted -_-
+    try:
+        print(file_path)
+        if(".wav" not in file_path):
+            # Step 1: convert to wav if not in the format
+            clip = VideoFileClip(file_path)
+            clip.audio.write_audiofile("out_audio.wav")
+            file_path="out_audio.wav"
+        y, sr = librosa.load(file_path) #sr sample rate
+        # some sample smaller than 30s
+        if len(y) < sr*30:
+            y = np.pad(y, (0, int(sr*30) - len(y)))
+        y = y[:int(sr*30)]
+        # Step 2: do fft over the audio file and convert output to mel scale
+        mel_spect = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=1024)
+        # # Convert amplitude to decibels so that difference in loudness is distinguishable
+        mel_spect = librosa.power_to_db(mel_spect, ref=np.max)
+        # getting all values between 0 and 1
+        mel_spect = (mel_spect - mel_spect.min()) / (mel_spect.max() - mel_spect.min())
+        return mel_spect
+    except Exception:
+        print("Audio corrupt.. skip")
+        return None
